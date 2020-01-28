0a67b5190df82fcd15dc6d1fc5c6e48b3d45bcc6
Nazir Hasan <nazirwork@gmail.com>
2020-01-28 20:27:15 +0530

    Adding a component and using it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Person from './Person/Person'
 import './App.css';
 
 class App extends Component {
@@ -6,8 +7,13 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello World</h1>
+        <Person></Person>
       </div>
     );
+
+    /** Line Number 7,8,9 10 gets compiles to line 14 */
+    // null is a configuration like classname
+   // return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'This is more magic'));
   }
 }
 
